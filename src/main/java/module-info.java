249fb0d746a91925dc093683cfb78aca3ac54932
FeliciaChoo <147573086+FeliciaChoo@users.noreply.github.com>
2025-01-03 11:31:46 +0800

    first commit
    dg

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.csc3202.lab.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.csc3202.lab.project to javafx.fxml;
+    exports com.csc3202.lab.project;
+}
