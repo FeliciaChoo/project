b25c727ab5bae29061ea3381aa7a22f33425bc1b
FarzanaAlya <nur.farzana.alya@gmail.com>
2025-01-10 11:58:00 +0800

    up1

--- src/main/java/com/csc3202/lab/project/HelloApplication.java
+++ src/main/java/com/csc3202/lab/project/HelloApplication.java
@@ -13,7 +13,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         stage.setTitle("Hanii100");
+=======
+        stage.setTitle("Hani");
+>>>>>>> Stashed changes
 =======
         stage.setTitle("Hani");
 >>>>>>> Stashed changes
